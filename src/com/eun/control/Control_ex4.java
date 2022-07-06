2eb826830d898f3834129215055d6b84fde3e234
EUNSH1 <tkdgus968@naver.com>
2022-07-07 08:58:31 +0900

    0707test

--- src/com/eun/control/Control_ex4.java
+++ src/com/eun/control/Control_ex4.java
@@ -8,37 +8,14 @@ public class Control_ex4 {
 		int num = sc.nextInt();
 		System.out.println("알파벳을 입력");
 		char ch = sc.next().charAt(0);
-		if(num==1) {
-			ch = (char)(ch+1);
+		if(num>=1 && num<=5) {
+			ch = (char)(ch+num);
 			if(ch >= 123){
 				ch = (char) (ch-26);
 			}
 			System.out.println(ch +"임");
 			
-		}else if (num==2) {
-			ch = (char)(ch+2);
-			if(ch >= 123){
-				ch = (char) (ch-26);
-			}
-			System.out.println(ch +"임");
-		}else if (num==3) {
-			ch = (char)(ch+3);
-			if(ch >= 123){
-				ch = (char) (ch-26);
-			}
-			System.out.println(ch +"임");
-		}else if (num==4) {
-			ch = (char)(ch+4);
-			if(ch >= 123){
-				ch = (char) (ch-26);
-			}
-			System.out.println(ch +"임");
-		}else if (num==5) {
-			ch = (char)(ch+5);
-			if(ch >= 123){
-				ch = (char) (ch-26);
-			}
-			System.out.println(ch +"임");
 		}
 	}
+	
 }
