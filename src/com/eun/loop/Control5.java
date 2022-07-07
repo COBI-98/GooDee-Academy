e07141a038c38b5b3b1d4e8d939dac399f8400bc
EUNSH1 <tkdgus968@naver.com>
2022-07-07 14:51:14 +0900

    KK

---
+++ src/com/eun/loop/Control5.java
@@ -0,0 +1,32 @@
+package com.eun.loop;
+
+import java.util.Scanner;
+
+public class Control5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char ss = 'c';
+		System.out.println((int)ss);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int sum = a+b+c;
+		int avg = sum/3;
+		int test = avg/10;
+		switch (avg) {
+		case  10:
+		case  9:
+			
+			
+			break;
+//ㅇㅇㅇㅇㅇㅇ
+		default:
+			break;
+		}
+		
+	}
+
+}
