ee09f15120e94de3c45a100b87772f6382ddaf32
EUNSH1 <tkdgus968@naver.com>
2022-07-07 12:14:55 +0900

    1214

--- src/com/eun/control/Control5.java
+++ src/com/eun/control/Control5.java
@@ -7,20 +7,20 @@ public class Control5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+		char ss = 'c';
+		System.out.println((int)ss);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		
 		int sum = a+b+c;
 		int avg = sum/3;
-		
+		int test = avg/10;
 		switch (avg) {
-		case 1:
-			if (avg>=90) {
-				System.out.println("총점은"+ sum+"입니다");
-				System.out.println("평균은" + avg + "이며 A학점입니다");
-			}
+		case  10:
+		case  9:
+			
+			
 			break;
 
 		default:
---
+++ src/com/eun/loop/Loop1.java
@@ -0,0 +1,20 @@
+package com.eun.loop;
+
+import java.util.Scanner;
+
+public class Loop1 {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Start");
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("End");
+	}
+
+}
