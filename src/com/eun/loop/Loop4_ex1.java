21fd31d20a638a11649c1d924779184d82f3f7d9
olrlobt <317tmdgjs@naver.com>
2022-07-07 17:48:51 +0900

    dd

--- src/com/eun/loop/Loop4_ex1.java
+++ src/com/eun/loop/Loop4_ex1.java
@@ -1,9 +1,18 @@
 package com.eun.loop;
 
+import java.util.Scanner;
+
 public class Loop4_ex1 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int mi = 0,si = 0;
+		System.out.println("분 입력");
+		mi = sc.nextInt();
+		System.out.println("초 입력");
+		si = sc.nextInt();
 		
 		for(int m=0;m<60;m++) {
 			
@@ -12,11 +21,11 @@ public class Loop4_ex1 {
 				
 				System.out.println(m + "분 : "+ s + "초");
 				
-				if(m==1&&s==15)break;
+				if(m==mi&&s==si)break;
 				
 				
 			}
-			if(m==1)break;
+			if(m==mi)break;
 		}
 		
 		
