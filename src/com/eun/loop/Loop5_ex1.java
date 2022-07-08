d27549d122bc717b0351f6845bb3c67962f9b73b
olrlobt <317tmdgjs@naver.com>
2022-07-08 12:06:48 +0900

    RPG game

--- src/com/eun/loop/Loop5_ex1.java
+++ src/com/eun/loop/Loop5_ex1.java
@@ -11,34 +11,83 @@ public class Loop5_ex1 {
 		int yId = 1234;
 		int yPw = 5678;
 		boolean check = true;
+		int lv = 1;
+		int exp = 0;
+		int gold = 0;
+		boolean logincheck = false ;
 		
 		
-			while(true) {
-				System.out.println("1. 로그인 2. 프로그램 종료");
-				int num = sc.nextInt();
-				
-				if(num == 1) {
-				}else {
-					System.out.println("종료");
-					check =! check;
-					break;
+		while(check) {
+			System.out.println("1. 로그인 2. 프로그램 종료");
+			int num = sc.nextInt();
+			
+			if(num == 1) {
+			}else {
+				System.out.println("종료");
+				check =! check;
+				break;
+			}
+			
+			System.out.println("아이디입력");
+			int checkId = sc.nextInt();
+			System.out.println("비밀번호입력");
+			int checkPw = sc.nextInt();
+			
+			if(yId!=checkId && yPw != checkPw) {
+				System.out.println("로그인실패");
+			}else {
+				System.out.println("로그인성공");
+				check =! check;
+				logincheck =! logincheck;
+				System.out.println("Game Start");
+				break;
 				}
+			}
+		
+		while(logincheck) {
+			
+			while(exp<(lv * 3)) {
 				
-				System.out.println("아이디입력");
-				int checkId = sc.nextInt();
-				System.out.println("비밀번호입력");
-				int checkPw = sc.nextInt();
+				System.out.println("사냥");
+				exp ++;
+
+			}
+			lv++;
+			exp = 0;
+			System.out.println("레벨 업");
+			
+			if( lv == 5) {
+							
+				System.out.println("5렙 ㅊㅋㅊㅋ 1000 Gold 획득");		
+				gold += 1000;
+							
+			}
+			if( lv == 10) {
 				
-				if(yId!=checkId) {
-					System.out.println("로그인실패");
-					}else if (yPw != checkPw) {
-					System.out.println("로그인실패");
-					}else {
-						System.out.println("로그인성공");
-						check =! check;
-						break;
-					}
-				}
+				System.out.println("10렙 ㅊㅋㅊㅋ 2000 Gold 획득");		
+				gold += 2000;
+				
+			}
+			
+			
+			if( lv == 15) {
+				
+				System.out.println("만렙 ㅊㅋㅊㅋ 3000 Gold 획득");		
+				gold += 3000;
+				break;
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+			
+			
 			} 
 			
 		}
---
+++ src/com/eun/loop/Loop5_ex2.java
@@ -0,0 +1,16 @@
+package com.eun.loop;
+
+public class Loop5_ex2 {
+	public static void main(String[] args) {
+	
+	
+	
+	
+		
+		
+		
+		
+	
+	
+	}
+}
