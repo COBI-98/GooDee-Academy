8f170a06a3ac00196239d58fe1b8e789791fbaf1
EUNSH1 <tkdgus968@naver.com>
2022-07-08 11:35:22 +0900

    login0707

---
+++ src/com/eun/loop/Loop5_ex1.java
@@ -0,0 +1,45 @@
+package com.eun.loop;
+import java.util.Scanner;
+
+public class Loop5_ex1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int yId = 1234;
+		int yPw = 5678;
+		boolean check = true;
+		
+		
+			while(true) {
+				System.out.println("1. 로그인 2. 프로그램 종료");
+				int num = sc.nextInt();
+				
+				if(num == 1) {
+				}else {
+					System.out.println("종료");
+					check =! check;
+					break;
+				}
+				
+				System.out.println("아이디입력");
+				int checkId = sc.nextInt();
+				System.out.println("비밀번호입력");
+				int checkPw = sc.nextInt();
+				
+				if(yId!=checkId) {
+					System.out.println("로그인실패");
+					}else if (yPw != checkPw) {
+					System.out.println("로그인실패");
+					}else {
+						System.out.println("로그인성공");
+						check =! check;
+						break;
+					}
+				}
+			} 
+			
+		}
+		
+		
