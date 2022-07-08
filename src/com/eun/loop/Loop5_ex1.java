c87652f9af290322786b1c418fd6571010c55462
EUNSH1 <tkdgus968@naver.com>
2022-07-08 16:07:16 +0900

    0708rr

--- src/com/eun/loop/Loop5_ex1.java
+++ src/com/eun/loop/Loop5_ex1.java
@@ -12,8 +12,11 @@ public class Loop5_ex1 {
 		int yPw = 5678;
 		boolean check = true;
 		boolean login = false;
+		int exp = 0;
+		int gold = 0;
 		
-			while(true) {
+		
+			while(check) {
 				System.out.println("1. 로그인 2. 프로그램 종료");
 				int num = sc.nextInt();
 				
@@ -32,21 +35,68 @@ public class Loop5_ex1 {
 				if(yId!=checkId) {
 					System.out.println("로그인실패");
 					}else if (yPw != checkPw) {
-					System.out.println("로그인실패");
-					}else {
+						System.out.println("로그인실패");
+					}
+					else {
 						System.out.println("로그인성공");
 						check =! check;
 						login =! login;
+						System.out.println("GameStart");
 						break;
 					}
 				}
-			
-			if (login == true ) {
-				for(int i=1; i<16 ; i++){
-					System.out.println(i+"레벨입니다");
+			if (login ==true ){
+				for (int lv = 1 ;lv<=15;lv++) {
+					System.out.println(lv+"입니다");
+					
+					
+					
+					exp=0;
+					if(lv%5 == 0) {
+						System.out.println(lv + "렙 ㅊㅋㅊㅋ 1000 Gold 획득");	
+					}
+					while(exp<(lv*3)) {
+						exp++;
+						System.out.println("사냥");
+					}
+					
 				}
 				
+				
 			}
+//			while (login == true ) {
+//				
+//				while(exp<(lv*3)) {
+//					System.out.println("사냥");
+//					exp++;
+//				}
+//				lv++;
+//				exp = 0;
+//				System.out.println("레벨업!"+lv+"레벨 입니다.");
+//			
+//				if( lv == 5) {
+//					
+//					System.out.println("5렙 ㅊㅋㅊㅋ 1000 Gold 획득");		
+//					gold += 1000;
+//								
+//				}
+//				if( lv == 10) {
+//					
+//					System.out.println("10렙 축하 2000 Gold 획득");		
+//					gold += 2000;
+//								
+//				}
+//				if( lv == 15) {
+//					
+//					System.out.println("15렙 축하합니다 3000 Gold 획득");		
+//					gold += 3000;
+//								
+//					
+//					System.out.println("만렙입니다.");
+//					break;
+//				}
+//			}
+//			
 	}
 }
 		
