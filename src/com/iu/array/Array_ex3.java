d0a3f05a59a68011202c2e0024c56f730fa77321
olrlobt <317tmdgjs@naver.com>
2022-07-11 16:53:08 +0900

    gogogo

--- src/com/iu/array/Array_ex3.java
+++ src/com/iu/array/Array_ex3.java
@@ -28,15 +28,15 @@ public class Array_ex3 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String[] student_name ;
-		int [] student_num ;
-		int [] student_kor ;
-		int [] student_eng;
-		int [] student_math ;
-		int [] student_total;
-		double [] student_avg ;
+		String[] student_name = new String[0] ;
+		int [] student_num = new int[0];
+		int [] student_kor = new int[0];
+		int [] student_eng = new int[0];;
+		int [] student_math  = new int[0];
+		int [] student_total = new int[0];
+		double [] student_avg  = new double[0];
 		
-		int number;
+		int number=0;
 		
 		
 		while(check) {
@@ -61,7 +61,7 @@ public class Array_ex3 {
 				
 				
 				for ( int i = 0 ; i < number ; i ++ ) {
-					System.out.println(i +"번째 학생 이름 :  ");
+					System.out.println(i+1 +"번째 학생 이름 :  ");
 					student_name[i] = sc.next();
 					System.out.println("학생 번호 :  ");
 					student_num[i] = sc. nextInt();
@@ -78,6 +78,13 @@ public class Array_ex3 {
 				
 			}else if (num == 2) {
 				
+				for (int i = 0 ; i < number ; i ++) {
+					
+					System.out.println(student_name[i] +" "+ student_num[i]+" "+student_kor[i]+" "+ student_eng[i]+" "+student_math[i]+" "+ student_total[i] +" "+student_avg[i]);
+					
+					
+					
+				}
 				
 				
 				
@@ -85,16 +92,150 @@ public class Array_ex3 {
 				
 			}else if (num == 3) {
 				
+				
+				
+				
+				
+				System.out.println("검색 가능한 학생 번호");
+				for (int i = 0 ; i < number ; i ++) {
+					
+					System.out.print(student_num[i]+" ");
+					
+				}
+				
+				System.out.println("검색할 번호 : ");
+				int j = sc.nextInt();
+				boolean checkst = false;
+				for (int i = 0 ; i < number ; i ++) {
+					
+					if(student_num[i] == j) {
+						System.out.println(student_name[i] +" "+ student_num[i]+" "+student_kor[i]+" "+ student_eng[i]+" "+student_math[i]+" "+ student_total[i] +" "+student_avg[i]);
+						checkst = true;
+						
+					}
+				}
+				
+				if(checkst == false ) {
+					System.out.println("그런 사람 없습니다. ");
+				}
+				
+				checkst = false;
+				
+				
 			}else if (num == 4) {
 				
+				System.out.println("삭제 가능한 학생 번호");
+				for (int i = 0 ; i < number ; i ++) {
+					
+					System.out.print(student_num[i]+" ");
+					
+				}
+				
+				System.out.println("삭제할 번호 : ");
+				int j = sc.nextInt();
+				boolean checkst = false;
+				int number1 = number;
+				
+				String student_name1[] = new String[number1];
+				int student_num1[] = new int[number1];
+				int student_kor1[] = new int[number1];
+				int student_eng1[] = new int[number1];
+				int student_math1[] = new int[number1];
+				int student_total1[] = new int[number1];
+				double student_avg1[] = new double[number1];
+				
+				int k = 0 ;
+				
+				for (int i = 0 ; i < number ; i ++) {
+					
+					if(student_num[i] == j) {
+						checkst = true;
+						
+						
+						continue;
+						
+						
+						
+					}
+					student_name1[k] = student_name[i];
+					student_num1[k] =student_num[i];
+					student_kor1[k] =student_kor[i];
+					student_eng1[k] =student_eng[i];
+					student_math1[k] =student_math[i];
+					student_total1[k] =student_total[i];
+					student_avg1[k] =student_avg[i];
+					
+					k++;
+				}
+				
+				if(checkst == false ) {
+					System.out.println("그런 사람 없습니다. ");
+				}else {
+					
+					number -= 1; 
+					student_name = student_name1;
+					student_num =student_num1;
+					student_kor =student_kor1;
+					student_eng =student_eng1;
+					student_math =student_math1;
+					student_total =student_total1;
+					student_avg =student_avg1;
+					
+					
+					
+					
+				}
+				
+				checkst = false;
+				
+				
 			}else if (num == 5) {
+				
+				
+				
+				number += 1;
+				String student_name1[] = new String[number];
+				int student_num1[] = new int[number];
+				int student_kor1[] = new int[number];
+				int student_eng1[] = new int[number];
+				int student_math1[] = new int[number];
+				int student_total1[] = new int[number];
+				double student_avg1[] = new double[number];
+				
+				System.out.println("추가할 학생 이름 :  ");
+				student_name1[number-1] = sc.next();
+				System.out.println("학생 번호 :  ");
+				student_num1[number-1] = sc. nextInt();
+				System.out.println("국어 점수 :   ");
+				student_kor1[number-1] = sc. nextInt();
+				System.out.println("영어 점수 :   ");
+				student_eng1[number-1] = sc. nextInt();
+				System.out.println("수학 점수 :   ");
+				student_math1[number-1] = sc. nextInt();
+				student_total1[number-1] = student_kor1[number-1] + student_eng1[number-1] + student_math1[number-1];
+				student_avg1[number-1] = student_total1[number-1]/3;
+				
+				
+				
+				
+				student_name = student_name1;
+				student_num =student_num1;
+				student_kor =student_kor1;
+				student_eng =student_eng1;
+				student_math =student_math1;
+				student_total =student_total1;
+				student_avg =student_avg1;
+				
+				
+				
+				
 				
 			}else {
 				break;
 			}
 		}
 	
-		
+		System.out.println("프로그램이 종료 되었습니다. ");
 		
 
 	}
