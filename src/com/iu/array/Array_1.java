21f395db47e0e8b8ef5b70bb0217e4e8247b3e6e
olrlobt <317tmdgjs@naver.com>
2022-07-11 10:55:30 +0900

    나였으면 1시에 왔따

---
+++ src/com/iu/array/Array_1.java
@@ -0,0 +1,27 @@
+package com.iu.array;
+
+public class Array_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int [] nums = new int[3];
+		char [] names = new char[2];
+		
+		
+		int age = 30 ;
+		nums[1] = age;
+		nums[2] = 50 ;
+		nums[0] = 10 ;
+		System.out.println(age);
+		System.out.println(nums);
+		System.out.println(nums[1]);
+		System.out.println(nums[0]);
+		System.out.println(nums[2]);
+
+		
+		
+	}
+
+}
