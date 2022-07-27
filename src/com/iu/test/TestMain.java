fe80c736288cf5cbb9d8288ea07b09bc2f5be0c7
EUNSH1 <tkdgus968@naver.com>
2022-07-27 12:22:57 +0900

    0727_12

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -1,10 +1,19 @@
 package com.iu.test;
 
+import com.iu.util.DBConnector;
+
 public class TestMain {
 
 	public static void main(String[] args) {
 		
+		DBConnector dbConnector = new DBConnector();
 		
+		try {
+			DBConnector.getConnetion();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
