8cc60854eb6dc8004a89664b4e48f0b9cb116152
EUNSH1 <tkdgus968@naver.com>
2022-08-03 14:32:14 +0900

    08031300

--- src/main/java/com/esh/start/test/DBConnectionTest.java
+++ src/main/java/com/esh/start/test/DBConnectionTest.java
@@ -1,18 +1,44 @@
 package com.esh.start.test;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import com.esh.start.member.BankMembersDAO;
+import com.esh.start.member.BankMembersDTO;
+import com.esh.start.member.BankMembersView;
 import com.esh.start.util.DBConnector;
 
 public class DBConnectionTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+		BankMembersDAO dao = new BankMembersDAO();
+		BankMembersDTO bankMembersDTO = new BankMembersDTO();
+		BankMembersView view = new BankMembersView();
+		Scanner sc = new Scanner(System.in);
+		ArrayList<BankMembersDTO> ar = new ArrayList<BankMembersDTO>();
+//		bankMembersDTO.setUSERNAME("EUN");
+//		bankMembersDTO.setPASSWORD("QQQ");
+//		bankMembersDTO.setNAME("SANGHYEON");
+//		bankMembersDTO.setEMAIL("QQQ@naver.com");
+//		bankMembersDTO.setPHONE("010-1234");
 		try {
-			Connection con  = DBConnector.getConnetion();
-			System.out.println(con != null);
+			
+			System.out.println("검색하고싶은 아이디를 입력하세요 ");
+			String str = sc.next();
+			ar = dao.getSearchByID(str);
+			
+			view.view(ar);
+			
+		
+//			if(result > 0) {
+//				System.out.println("성공");
+//			}else {
+//				System.out.println("실패");
+//			}
+//			Connection con  = DBConnector.getConnetion();
+//			System.out.println(con != null);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
