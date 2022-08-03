0fe5d02f7ace16613de0e9a601146fd94d2fece4
EUNSH1 <tkdgus968@naver.com>
2022-08-03 11:09:07 +0900

    testgood

--- src/main/java/com/esh/start/test/DBConnectionTest.java
+++ src/main/java/com/esh/start/test/DBConnectionTest.java
@@ -1,10 +1,24 @@
 package com.esh.start.test;
 
+import java.sql.Connection;
+
+import com.esh.start.util.DBConnector;
+
 public class DBConnectionTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		try {
+			Connection con  = DBConnector.getConnetion();
+			System.out.println(con != null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
