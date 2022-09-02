269ec40074dbd541f541126ebc10ce328aa9decf
EUNSH1 <tkdgus968@naver.com>
2022-09-02 10:48:54 +0900

    0902

--- src/main/webapp/resources/js/board.js
+++ src/main/webapp/resources/js/board.js
@@ -2,48 +2,24 @@
 // let kind = '${param.kind}';
 // alert(kind);
 
-const cb = document.getElementsByClassName("cb");
-const check_all = document.getElementById("check_all");
-const btn_check = document.getElementById("btn_check");
-const frm = document.getElementById("frm");
-
-check_all.addEventListener("click",function(){
-
-    for(let i=0; i<cb.length;i++){
-       
-            cb[i].checked = check_all.checked;
-    }
-   
-    
-});
-
-btn_check.addEventListener("click",function(){
-    let result = true;
-    console.log(cb[1].checked)
-    for(let i =0; i<cb.length;i++){
-        if(!cb[i].checked){
-            alert("약관 동의 바람");
-            result = false;
-            break;
-        }
-    }
-
-    if(result ==true){
-    frm.submit();
-    }
-});
-
-
-
-for(let i=0;cb.length;i++){
-    
-    // cb[i].addEventListener("click",function(){
-    //     check(1);
-    // });
-    cb[i].addEventListener("click", check);
-};
 
 function check(){
+    const cb = document.getElementById("cb");
+    const check_all = document.getElementById("check_all");
+
+    // const cb = document.querySelectorAll(".cb");
+    // const check_all = document.querySelector("#check_all");
+    const btn_check = document.getElementById("btn_check");
+    const frm = document.getElementById("frm");
+
+
+    console.log("foreach");
+
+
+    // cb.forEach(function(v,i,ar){
+    //     console.log(v);
+    // });
+
     let result = true;
     for(let i=0;i<cb.length;i++){
         if(!cb[i].checked){
@@ -52,4 +28,40 @@ function check(){
         }
     }
     check_all.checked =result;
+
+    check_all.addEventListener("click",function(){
+
+        for(let i=0; i<cb.length;i++){
+           
+                cb[i].checked = check_all.checked;
+        }
+       
+        
+    });
+    
+    btn_check.addEventListener("click",function(){
+        let result = true;
+        console.log(cb[1].checked)
+        for(let i =0; i<cb.length;i++){
+            if(!cb[i].checked){
+                alert("약관 동의 바람");
+                result = false;
+                break;
+            }
+        }
+    
+        if(result ==true){
+        frm.submit();
+        }
+    });
+    
+    
+    
+    for(let i=0;cb.length;i++){
+        
+        // cb[i].addEventListener("click",function(){
+        //     check(1);
+        // });
+        cb[i].addEventListener("click", check);
+    };
 }
